855cf72bd9bf3b275a29c03fff0c66a711e67c54
huynhminhkhai <150777715+huynhminhkhai@users.noreply.github.com>
2024-11-04 13:16:52 +0700

    xong phần welcome

--- app/src/main/java/com/khai/dev/mvvmappshop/ui/screen/auth/WelcomeScreen.kt
+++ app/src/main/java/com/khai/dev/mvvmappshop/ui/screen/auth/WelcomeScreen.kt
@@ -1,46 +1,146 @@
 package com.khai.dev.mvvmappshop.ui.screen.auth
 
-import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
+import com.khai.dev.mvvmappshop.R
 import com.khai.dev.mvvmappshop.models.welcome.WelcomeModel
 import com.khai.dev.mvvmappshop.viewmodel.WelcomeViewModel
-import kotlin.math.log
 
 @Composable
-fun WelcomeScreen(welcomeViewModel: WelcomeViewModel = viewModel()) {
+fun WelcomeScreen(
+    welcomeViewModel: WelcomeViewModel = viewModel()
+) {
     // Quan sát LiveData từ ViewModel
     val welcomes by welcomeViewModel.welcomes.observeAsState(initial = emptyList())
 
+    // Lấy một welcome ngẫu nhiên
+    val randomWelcome = welcomes.randomOrNull() // Trả về null nếu danh sách rỗng
 
-    LazyColumn {
-        items(welcomes) { welcomeItem ->
-            WelcomeItem(welcomeItem)
+    // Hiển thị welcome nếu có
+    randomWelcome?.let { WelcomeItem(it) }
+}
+
+
+
+@Composable
+fun WelcomeItem(welcomeModel: WelcomeModel) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(welcomeModel.fullImageUrl),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .height(400.dp)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = "MAKE YOUR", 
+                    fontSize = 24.sp,
+                    color = colorResource(id = R.color.gray),
+                    fontWeight = FontWeight(600),
+                    fontFamily = FontFamily(Font(R.font.gelasio_bold))
+                )
+                Text(
+                    text = welcomeModel.title,
+                    fontSize = 30.sp,
+                    color = colorResource(id = R.color.black),
+                    fontWeight = FontWeight(700),
+                    fontFamily = FontFamily(Font(R.font.gelasio_bold))
+                )
+                Text(
+                    modifier = Modifier.padding(start = 25.dp, top = 15.dp),
+                    text = "The best simple place where you discover most wonderful furnitures and make your home beautiful",
+                    color = colorResource(id = R.color.graySecond),
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily(Font(R.font.nunitosans_7pt_condensed_light)),
+                    lineHeight = 35.sp,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column {
+                CustomRow(
+                    onClick = {
+                    })
+                Spacer(modifier = Modifier.height(20.dp))
+            }
         }
     }
 }
 
 @Composable
-fun WelcomeItem(welcomeModel: WelcomeModel) {
-//    val fullImageUrl = "http://172.16.106.205:1402/api/welcomes" + welcomeModel.imageUrl
-    Log.d("LinkImage", "Đường dẫn ảnh: ${welcomeModel.fullImageUrl}")
-    Text(text = welcomeModel.title)
-    Text(text = welcomeModel.description)
-    Image(
-        painter = rememberAsyncImagePainter(welcomeModel.fullImageUrl),
-        contentDescription = welcomeModel.description,
-                modifier = Modifier.size(200.dp)
-    )
-    // Thêm các thành phần khác để hiển thị thông tin của `welcomeModel` nếu cần
+fun CustomRow(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .padding(7.dp)
+            .width(160.dp)
+            .height(60.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(Color(0xFF242424))
+            .clickable (onClick = onClick)
+            .then(modifier)
+    ) {
+        Row (
+            modifier = Modifier.fillMaxSize(),  // Fill the size of the Box
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Get Started",
+                color = Color.White,
+                fontFamily = FontFamily(Font(R.font.gelasio_bold)),
+                fontWeight = FontWeight(600),
+                fontSize = 18.sp
+            )  // Set text color
+        }
+    }
 }
